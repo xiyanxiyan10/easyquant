fa4e673fd01c1783ac7b6b11cb7c932b78a84904
梅梅 <xiyanxiyan10@hotmail.com>
2024-06-16 23:54:46 +0800

    dynamic config for policy

--- crypto/hudgegrip.py
+++ crypto/hudgegrip.py
@@ -29,15 +29,15 @@ with open('./token.json', 'r') as f:
 if config_params["backtest"]["mode"] == 'True':
     backtest_mode = True
 
-import pdb
-pdb.set_trace()
 mail_address = config_params["mail"]["address"]
 mail_password = config_params["mail"]["password"]
 mail_period = int(config_params["mail"]["period"])
 event_period = EventPeriod(mail_period)
+symbol_tuple = config_params["live"]["datas"]
+backfile_array = config_params["backtest"]["datas"]
+backfile_path = config_params["backtest"]["path"]
 
 ## 0 is long and 1 is short
-symbol_tuple = ('ETH/USDT', 'SOL/USDT')
 price_tuple = (3659.19, 158.55)
 
 class Hudge_Indicator(bt.Indicator):
@@ -91,23 +91,20 @@ class HudgeGripStrategy(bt.Strategy):
             # Slow things down.
         #    cash = 'NA'
 
-        long_price = -1
-        short_price = -1
         output = ''
         for data in self.datas:
             output += ('{} - {} | O: {} H: {} L: {} C: {} V:{}\n'.format(data.datetime.datetime(),
                                                                                    data._name, data.open[0], data.high[0], data.low[0], data.close[0], data.volume[0],
                                                                                    ))
-        if data._name == symbol_tuple[0]:
-            long_price = data.close[0]
-
-        if data._name == symbol_tuple[1]:
-            short_price = data.close[0]
+        long_price = self.data0.close[0]
+        short_price = self.data1.close[0]
 
         output += ("long price: %f, shot price %f\n" % (long_price, short_price))
         output += ("%f %%\n" % (self.hudge_Indicator.relative_volatility[0]))
         print(output)
         if not backtest_mode and event_period.check():
+            import pdb
+            pdb.set_trace()
             qq_mail_send(mail_address, [mail_address], mail_password, 'hudgegride', output)
             print("send email notify")
 
@@ -148,15 +145,12 @@ broker_mapping = {
 
 
 def run_backtest():
-    long_data = csv_to_btdata('./eth.csv')
-    short_data = csv_to_btdata('./sol.csv')
-
     cerebro = bt.Cerebro()
+    for i in range(len(backfile_array)):
+        back_data = csv_to_btdata(backfile_path + backfile_array[i] + ".csv")
+        cerebro.adddata(back_data, name=backfile_array[i])
+
     cerebro.addstrategy(HudgeGripStrategy)
-
-    cerebro.adddata(long_data, name=symbol_tuple[0])
-    cerebro.adddata(short_data, name=symbol_tuple[1])
-
     cerebro.run()
     cerebro.plot()
 
@@ -167,20 +161,21 @@ def run_live():
     cerebro.addstrategy(HudgeGripStrategy)
 
     # Create our store
-    config = {'apiKey': config_params["binance"]["apikey"],
-          'secret': config_params["binance"]["secret"],
+    config = {'apiKey': config_params["live"]["apikey"],
+          'secret': config_params["live"]["secret"],
           'enableRateLimit': True,
           'options': {
-            'defaultType': 'future',
+            'defaultType': config_params["live"]["type"],
           },
-          'proxies': {'https':"http://127.0.0.1:7890", 'http':"http://127.0.0.1:7890"}
+          'proxies': {'https':config_params["live"]["proxy"], 'http':config_params["live"]["proxy"]}
           }
+
 
 
     # IMPORTANT NOTE - Kraken (and some other exchanges) will not return any values
     # for get cash or value if You have never held any BNB coins in your account.
     # So switch BNB to a coin you have funded previously if you get errors
-    store = CCXTStore(exchange='binance', currency='BNB', config=config, retries=5, debug=False)
+    store = CCXTStore(exchange=config_params["live"]["exchange"], currency=config_params["live"]["currency"], config=config, retries=5, debug=False)
 
     broker = store.getbroker(broker_mapping=broker_mapping)
     cerebro.setbroker(broker)
@@ -189,17 +184,13 @@ def run_live():
     # Drop newest will prevent us from loading partial data from incomplete candles
     hist_start_date = datetime.utcnow() - timedelta(minutes=50)
 
-    short_data = store.getdata(dataname=symbol_tuple[1], name=symbol_tuple[1],
+    for i in range(len(symbol_tuple)):
+        live_data = store.getdata(dataname=symbol_tuple[i], name=symbol_tuple[i],
                      timeframe=bt.TimeFrame.Minutes, fromdate=hist_start_date,
                      compression=1, ohlcv_limit=50, drop_newest=True) #, historical=True)
 
-    long_data = store.getdata(dataname=symbol_tuple[0], name=symbol_tuple[0],
-                     timeframe=bt.TimeFrame.Minutes, fromdate=hist_start_date,
-                     compression=1, ohlcv_limit=50, drop_newest=True) #, historical=True)
-
-    # Add the feed
-    cerebro.adddata(long_data, name = symbol_tuple[0])
-    cerebro.adddata(short_data, name = symbol_tuple[1])
+        # Add the feed
+        cerebro.adddata(live_data, name = symbol_tuple[i])
 
     # Run the strategy
     cerebro.run()
