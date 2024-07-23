5f51d5eb5f5e007972e25829a0752b7f48d813ed
梅梅 <xiyanxiyan10@hotmail.com>
2024-07-23 10:20:31 +0800

    更灵活的数据清洗脚本

--- tools/tokeninsight.py
+++ tools/tokeninsight.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import argparse
 from datetime import datetime
 from dateutil import parser
+import sys
 
 import pdb
+
 
 def timestamp2day(timestamp):
     datetime_obj = parser.parse(timestamp)
@@ -23,7 +26,17 @@ def check_null(df):
     num_null_rows = len(null_rows)
     return num_null_rows
 
-df = pd.read_csv('./input.csv',encoding= 'utf-8',header=None)
+
+def parse_args():
+    parse = argparse.ArgumentParser(description='input origin csv file, output clean csv file')  # 2、创建参数对象
+    parse.add_argument('-i', '--input', default='input.csv', type=str, help='input csv file path')  # 3、往参数对象添加参数
+    parse.add_argument('-o', '--output', default='output.csv', type=str, help='output csv file path')
+    args = parse.parse_args()  # 4、解析参数对象获得解析对象
+    return args
+
+args = parse_args()
+
+df = pd.read_csv(args.input, encoding= 'utf-8',header=None)
 #pdb.set_trace()
 
 df =  set_columns(df)
@@ -36,4 +49,4 @@ if missing_count > 0:
     print("missing count:%d" % missing_count)
 else:
     print("generate output csv")
-    df.to_csv('output.csv', index=False, header=False)
+    df.to_csv(args.output, index=False, header=False)
